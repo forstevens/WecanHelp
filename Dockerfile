d1c8a2a941a4e919d01b965f8b11983dfd2bd744
孙铭聪 <stevens@stu.xjtu.edu.cn>
2018-06-24 03:44:45 +0800

    remove

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM registry.op.tiaozhan.com/tz-php7:latest
-
-MAINTAINER dongjiangbin "dongjiangbin@tiaozhan.com"
-
-COPY [".", "/runtime"]
-
-RUN /usr/sbin/cbuild
--- run.bat
+++
@@ -1,5 +0,0 @@
-php think apply3:drop
-php think migrate:run
-php think apply3:import *
-php think apply3:export place APP/public/static/place.json auto
-php think apply3:export resource APP/public/static/resource.json auto
